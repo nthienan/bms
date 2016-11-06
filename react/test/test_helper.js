a9431dd1bfffb96c64ab4e13a2c82ad103380b3f
An Nguyen <thienan93@yahoo.com>
2016-11-06 11:27:09 +0700

    Module gradle build and add travis

--- react/test/test_helper.js
+++ react/test/test_helper.js
@@ -8,7 +8,7 @@ import chai, { expect } from 'chai';
 import chaiJquery from 'chai-jquery';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducers from '../src/reducers/index';
+import reducers from '../src/reducers/index-reducer';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
